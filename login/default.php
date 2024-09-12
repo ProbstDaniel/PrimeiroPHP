93a2de5ef57ab733f643fe825c84a35e0d9a8513
ProbstDaniel <danelprobst@gmail.com>
2024-09-12 11:45:44 -0300

    Primeira aula PHP

---
+++ login/default.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+    <?php
+    echo "PrimeiroPHP!<br>";
+    $nome = "Gabriel Kreischer! ";
+    echo $nome;
+    $MeuNome = "Daniel";
+    $MeuSobrenome = "Probst";
+    echo "$MeuNome"," $MeuSobrenome";
+    ?>
+    
+
+</body>
+</html>
